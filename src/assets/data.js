bf711bbc665bb0eb56b01475f06a35321f0e2d14
Kanchan154 <km536389@gmail.com>
2025-04-30 22:00:21 +0530

    Quiz_App

---
+++ src/assets/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        question: "which device is required for the Internet connection?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "LAN Cable",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+    {
+        question: "which device is required for the Internet connection?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "LAN Cable",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+    {
+        question: "which device is required for the Internet connection?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "LAN Cable",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+    {
+        question: "which device is required for the Internet connection?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "LAN Cable",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+]
